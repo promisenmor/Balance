242f7339bf4cbb195316bcd7def10cf8ee28355a
promisenmor <nmorchibogu98@gmail.com>
2025-05-27 22:00:09 +0100

    finished the core.models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,83 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator
+from django.conf import settings
 
-# Create your models here.
+
+class User(AbstractUser):
+    full_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    is_verified = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
+    
+
+class Wallet(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,)
+    bank_name = models.CharField(max_length=100)
+    account_number = models.IntegerField()
+    balance = models.DecimalField(max_digits=16, decimal_places=2, default=0.00)
+    linked = models.BooleanField(default=False)
+
+
+
+class Transaction(models.Model):
+    TRANSACTION_TYPE = [
+        ('income', 'Income'),
+        ('expense', 'Expense'),
+    ]
+
+    CATEGORY_CHIOCES = [
+        ('food', 'Food'),
+        ('transportation', 'Transportation'),
+        ('entertainment', 'Entertainment'),
+        ('data', 'Data'),
+        ('salary', 'Salary'),
+        ('other', 'Other'),
+    ]
+
+    wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=12, decimal_places=2)
+    categories = models.CharField(max_length=50, choices=CATEGORY_CHIOCES)
+    type = models.CharField(max_length=10, choices=TRANSACTION_TYPE)
+    description = models.TextField(blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.type} - {self.amount}"
+    
+
+class Budget(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    category = models.CharField(max_length=50, choices=Transaction.CATEGORY_CHIOCES)
+    amount = models.DecimalField(max_digits=12, decimal_places=2)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.user.email} -  {self.category}"
+    
+
+class SavingGoal(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title  = models.CharField(max_length=100)
+    target_amount = models.DecimalField(max_digits=12, decimal_places=2)
+    saved_amount = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+    deadline = models.DateField()
+
+    def __str__(self):
+        return f"{self.title} - {self.user.email}"
+    
+
+class Notification(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    message = models.TextField()
+    seen = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.email} - {self.message[:30]}"
